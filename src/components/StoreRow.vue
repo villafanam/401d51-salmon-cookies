bf0d21dd1a99b98e6ae2d444bffb31e15ff463d9
Marco <pastry07@gmail.com>
2023-04-13 01:07:28 -0700

    fixed props issue

--- src/components/StoreRow.vue
+++ src/components/StoreRow.vue
@@ -1,56 +1,45 @@
 <script>
-// defineProps({
-//   store: {
-//     type: Object,
-//     required: true
-//   },
-//   headRW: {
-//     type: Array,
-//     required: true
-//   }
-// })
 
 export default {
-  props:['store', 'headRW'],
-  // setup(props){console.log(test)},
+  props: ["store"],
   data() {
     return {
       // headRW: ['Store', '6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', 'DL Total'],
-      columns: new Array(14).fill(''),
-      // store:
-      // {
-      //   name: 'Seattle',
-      //   min: 23,
-      //   max: 65,
-      //   avg: 6.3
-      // }
-      // ,
+      // store: [{ name: "Seattle", min: 23, max: 65, avg: 6.3 }, { name: "Richland", min: 23, max: 65, avg: 6.3 }],
+      columns: new Array(14).fill(0),
+      colTotal: new Array(14).fill(0),
       totalDT: 0,
     }
   },
   computed: {
     getRandomCust: function () {
+      // console.log('columns:', this.columns);
+      let randCus = 0;
+      // this.totalDT = 0;
       for (let i in this.columns) {
-        this.columns[i] = Math.floor(Math.random() * (this.store.max - this.store.min + 1)) + this.store.min;
+        randCus = Math.floor(Math.random() * (this.store.max - this.store.min + 1)) + this.store.min;
+        this.columns[i] = Math.floor(randCus * this.store.avg);
         this.totalDT += this.columns[i];
+        //this.colTotal[i] += this.columns[i];
       }
+      // console.log('columns:', this.columns);
     },
   },
-  mounted(){
-    this.getRandomCust();
-    // console.log('props:-----', props);
-  }
+  // mounted(){
+  //   // console.log('props:-----', this.store);
+  //   this.getRandomCust();
+  // }
 }
 
 </script>
 
 <template>
-  <!-- {{ getRandomCust }} -->
+  {{ getRandomCust }}
+
   <tr>
     <th>{{ store.name }}</th>
-    <td v-for="element in columns">
-      {{ element }}
-    </td>
+    <td v-for="element in columns">{{ element }}</td>
     <td>{{ totalDT }}</td>
   </tr>
+  <!-- <p>{{ this.colTotal }}</p> -->
 </template>
--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,62 +1,34 @@
 <script>
 import StoreRow from './StoreRow.vue';
 
-// defineProps({
-//   msg: {
-//     type: String,
-//     required: true
-//   }
-
-// })
-
-const headRW = ["Store", "6:00am", "7:00am", "8:00am", "9:00am", "10:00am", "11:00am", "12:00pm", "1:00pm", "2:00pm", "3:00pm", "4:00pm", "5:00pm", "6:00pm", "7:00pm", "DL Total"];
-const store = [{ name: "Seattle", min: 23, max: 65, avg: 6.3 }, { name: "Richland", min: 23, max: 65, avg: 6.3 }];
 
 export default {
-  // props: [headRW, store],
-  // setup(props) {
-  //   console.log(props.store);
-  // },
+  // props: ["store"],
   data() {
     return {
       headRW: ["Store", "6:00am", "7:00am", "8:00am", "9:00am", "10:00am", "11:00am", "12:00pm", "1:00pm", "2:00pm", "3:00pm", "4:00pm", "5:00pm", "6:00pm", "7:00pm", "DL Total"],
-      // columns: new Array(14).fill(""),
-      store: [{ name: "Seattle", min: 23, max: 65, avg: 6.3 }, { name: "Richland", min: 23, max: 65, avg: 6.3 }],
-      // totalDT: 0,
+      store: [
+        { name: "Seattle", min: 23, max: 65, avg: 6.3 },
+        { name: "Tokyo", min: 3, max: 24, avg: 1.2 },
+        { name: "Dubai", min: 11, max: 38, avg: 3.7 },
+        { name: "Paris", min: 20, max: 38, avg: 2.3 },
+        { name: "Lima", min: 2, max: 16, avg: 4.6 },
+      ],
     };
   },
-  // computed: {
-  //     getRandomCust: function () {
-  //         // let results =[];
-  //         for (let i in this.columns) {
-  //             this.columns[i] = Math.floor(Math.random() * (this.store.max - this.store.min + 1)) + this.store.min;
-  //             this.totalDT += this.columns[i];
-  //         }
-  //         // return results;
-  //     },
-  //     // renderData: function (stores) {
-  //     //   this.totalDT++;
-  //     //   return stores.map(function (store, idx) {
-  //     //     return <th>{{ getRandomCust(store.min, store.max) }}</th>;
-  //     //   });
-  //     // }
-  // },
-  // components: { StoreRow }
+  components: { StoreRow }
 }
 
 </script>
 
 
-
 <template>
   <table id="cookie-stand">
     <tr>
-
       <th v-for="element in headRW">{{ element }}</th>
-
     </tr>
 
-    <StoreRow :store="store[0]" :headRW="headRW" />
+    <StoreRow v-for="element in store" :store="element" />
 
   </table>
 </template>
